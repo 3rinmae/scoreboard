1ab4c186a946f7b1253b385610db4f3483d5a840
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-09-28 19:57:54 -0600

    after tutoring

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 let home = 0
 let away = 0
 
+// this function take the home variable and adds one 
 function homePoints() {
+  // console.log('this is home score right now', home);
+
+  // grab home variable and use js to add 1
   home += 1;
+  // Log whenever you change or update a variable
+  // did the variable change as expected? Log it
   console.log(`home score: ${home}`);
-  const userInputElement = document.getElementById('homePoints')
-  userInputElement.innerText = home
+  // if the above console is working as intended, now show the change visibly on the page
+
+  // define an element that Im going to use to put this score on the page
+
+  // homeScoreElement 
+  // const userInputElement = document.getElementById('homePoints')
+  // userInputElement.innerText = home
+  drawPoints()
 }
+
+// This function just talks to my index and puts the variables where they should go based on the Id's
+function drawPoints() {
+  const homeScoreElm = document.getElementById('homePoints')
+  homeScoreElm.innerText = home
+
+  const awayScoreElm = document.getElementById('awayPoints')
+  awayScoreElm.innerText = away
+}
+
 
 function awayPoints() {
   away += 1;
   console.log(`away score: ${away}`);
-  const userInputElement = document.getElementById('awayPoints')
-  userInputElement.innerText = away
+  // const userInputElement = document.getElementById('awayPoints')
+  // userInputElement.innerText = away
+  drawPoints()
 }
 
 function homePointsThree() {
   home += 3;
+  // ALWAYS log this variable before putting stuff on the page
   console.log(`home score: ${home}`);
-  const userInputElement = document.getElementById('homePointsThree')
-  userInputElement.innerText = home
+  drawPoints()
 }
 
 function awayPointsThree() {
